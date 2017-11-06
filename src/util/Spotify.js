c6b13097b96fc269aea9ab4e44aff107e8945e41
liwia <32197906+liwia@users.noreply.github.com>
2017-11-06 07:49:45 +0100

    Update Spotify.js

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -19,7 +19,7 @@ const Spotify = {
       window.history.pushState('Access Token', null, '/');
       // This clears the parameters, allowing us to grab a new access token when it expires.
       return accessToken;
-      //should I return accessToken?
+
 }
     else{
       const url = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
@@ -28,19 +28,17 @@ const Spotify = {
 
 },
   search(term){
-      //return new Promise(resolve => resolve(term));?
 
       const accessToken = Spotify.getAccessToken();
       return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
     headers: { Authorization: `Bearer ${accessToken}`}
   }).then(response => {
-    if(response.ok){
-      return response.json();}
+      return response.json();
   }).then(jsonResponse => {
-    if(!jsonResponse.playlistTracks){
+    if(!jsonResponse.tracks){
       return [];
     }
-    return jsonResponse.playlistTracks.items.map(track => ({
+    return jsonResponse.tracks.items.map(track => ({
         id: track.id,
         name: track.name,
         artist: track.artists[0].name,
