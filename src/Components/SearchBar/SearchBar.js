eee35e92f63f1739a634baba56c390f352e1135e
liwia <32197906+liwia@users.noreply.github.com>
2017-11-07 17:44:39 +0100

    Update SearchBar.js

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -19,7 +19,7 @@ class SearchBar extends React.Component {
     this.props.onSearch(this.state.term);
   }
 
-  render (){
+  render(){
     return (
       <div className="SearchBar">
         <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange}/>
