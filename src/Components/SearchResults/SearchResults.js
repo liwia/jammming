f72756be9df1f73ee39f69d4257378a27c4bbbad
liwia <32197906+liwia@users.noreply.github.com>
2017-11-07 17:45:29 +0100

    Update SearchResults.js

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -3,7 +3,7 @@ import './SearchResults.css';
 import TrackList from '../TrackList/TrackList';
 
 class SearchResults extends React.Component {
-  render (){
+  render() {
     return (
       <div className="SearchResults">
         <h2>Results</h2>
